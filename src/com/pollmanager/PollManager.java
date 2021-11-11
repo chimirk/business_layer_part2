12fa627d3ed610b1914e15a1e4f83a3f5bd8aa76
Pierrick Catalo <43609852+PierrickPro@users.noreply.github.com>
2021-11-11 11:00:40 -0500

    Update PollManager.java

--- src/com/pollmanager/PollManager.java
+++ src/com/pollmanager/PollManager.java
@@ -15,7 +15,7 @@ public class PollManager {
         return poll;
     }
 
-    public void createPoll(String title, String question, ArrayList<Choice> choices, String userID) throws PollManagerException, PollException {
+    public void createPoll(String title, String question, ArrayList<Choice> choices, String creatorID) throws PollManagerException, PollException {
         Poll newPoll = new Poll();
 
         if (title.trim().isEmpty()) {
@@ -31,11 +31,11 @@ public class PollManager {
             }
         }
 
-        newPoll = new Poll(title, question);
+        newPoll = new Poll(title, question, creatorID);
         newPoll.setChoices(choices);
         newPoll.setStatus(PollStatus.CREATED);
         PollDAO pollDAO = new PollDAO();
-        pollDAO.insertPoll(newPoll, userID);
+        pollDAO.insertPoll(newPoll, creatorID);
     }
 
     public void deletePoll(String userId, String pollId) throws PollManagerException {
